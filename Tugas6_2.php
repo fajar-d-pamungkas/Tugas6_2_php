b04651ff29422f966ea0fa81319d15d8504a27c2
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-10-08 11:15:16 +0700

    Add files via upload

---
+++ Tugas6_2.php
@@ -0,0 +1,13 @@
+<?php
+$jambu = 15000;
+$dus = 2000;
+
+$jual = (6 * 5 * $jambu)+(2 * $dus);
+
+echo "============================================== <br>";
+echo "Harga Jambu = Rp $jambu / Kg <br>";
+echo "Harga Kardus = Rp $dus / pcs <br>";
+echo "Total Penjualan (Dus dan Jambu)= Rp $jual <br>";
+echo "============================================== <br>";
+
+ ?>
